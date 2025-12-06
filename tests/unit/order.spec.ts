355faddce0cb7da59feaab6192c5247553382931
Wellersom <wellersom.wrv@gmail.com>
2025-12-06 15:05:52 -0300

    adicionado gerador de uuid para teste

--- tests/unit/order.spec.ts
+++ tests/unit/order.spec.ts
@@ -1,12 +1,14 @@
 import { describe, it, expect } from '@jest/globals';
 import Order from '../../src/domain/entity/order';
 import { ErrorOrder } from '../../src/core/errors/error-order.ts';
+import { randomUUID } from 'crypto';
 
 describe('Entidade do Pedido', () => {
     it('deve lançar um erro quando o pedido estiver vazio', () => {
         expect(() => {
-            let myOrder = new Order(1, []);
+            let myOrder = new Order(randomUUID(), []);
             expect(myOrder).toThrow(ErrorOrder.EmptyItems());
         })
     });
+
 });
