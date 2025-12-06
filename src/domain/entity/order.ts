da33f3c701e215cacc2635b9bba9912fc5028899
Wellersom <wellersom.wrv@gmail.com>
2025-12-06 14:54:55 -0300

    adicionado geração de id por uuid

--- src/domain/entity/order.ts
+++ src/domain/entity/order.ts
@@ -1,12 +1,14 @@
+const { v4: uuid } = require("uuid");
+import type { UUID } from "crypto";
 import OrderItem from "./order_item.js";
 
 export default class Order {
-    _id: string;
+    _id: UUID;
     _customerId: number;
     _items: OrderItem[];
     
-    constructor(id: string, customerId: number, items?: OrderItem[]) {
-        this._id = id;
+    constructor(customerId: number, items?: OrderItem[]) {
+        this._id = uuid();
         this._customerId = customerId;
         this._items = items || [];
     }
